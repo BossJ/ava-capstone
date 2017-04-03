c6d5672df50478f53e89ad57d90292effe289ee4
shuchangbruce <shuchangbruce@gmail.com>
2017-04-02 21:26:33 -0700

    delete iframe, add webchat change color

--- ava-bot/bot-front-end/index.js
+++ ava-bot/bot-front-end/index.js
@@ -1,38 +1,5 @@
 $(function() {
     "use strict";
-    /*
-    q&a maker connection sameple:
-     POST /knowledgebases/de7047e0-2e03-4d9a-9dcc-757106fadcbb/generateAnswer
-     Host: https://westus.api.cognitive.microsoft.com/qnamaker/v1.0
-     Ocp-Apim-Subscription-Key: b1f0b2684e2946879f409b65f8590da6
-     Content-Type: application/json
-     {"question":"hi"}
-     */
-    $("#s-button").on("click", function (e) {
-        e.preventDefault(); //stop submit button reload the page
-        //console.log($("#first_input").val());
-        var input = {"question": $("#first_input").val()};
-        console.log(input); //log input object value
-        $.ajax({
-            url : "https://westus.api.cognitive.microsoft.com/qnamaker/v1.0/" +
-            "knowledgebases/de7047e0-2e03-4d9a-9dcc-757106fadcbb/generateAnswer",
-            type: "POST",
-            data : JSON.stringify(input),
-            headers:{
-                "Ocp-Apim-Subscription-Key": "b1f0b2684e2946879f409b65f8590da6"
-            },
-            contentType : "application/json",
-            success: function (d) {
-                console.log(d);
-                $("#message").text("Answer: " + d.answer + " , Score: " + d.score);
-            },
-            error: function (e) {
-                console.log(e);
-                $("#message").text("Error.   status code: " + e.status + ", status text: " + e.statusText + ", messsage: " +
-                e.responseText);
-            }
-        });
 
-    });
 
 });
